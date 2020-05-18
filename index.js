aba729a340d051de7fd86a1c539486763f21cb75
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-18 23:04:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { VK } = require('vk-io');
 
 const vk = new VK({
-	token: 'ddaf745cabae36fc51dab5569f4cad9d7ac2f862aba7ab8f5a0db88f4dfb3a89844618bb6d825b8102968'
+	token: 'TOKEN'
 });
 
 vk.updates.hear(/hello/i, context => (
