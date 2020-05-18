47fa766af03d947381348d5b6608cecb0e1aa000
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-18 23:54:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const { VK } = require('vk-io');
+const db = require("./db.json");
 
 const vk = new VK({
 	token: process.env.TOKEN
 });
+updates.use(async (context, next) => {
 
+    if (context.is("message") && context.isOutbox){
+        return;
+    }
+
+    if (!db[context.senderId]) {
+    	let user = await vk.api.users.get({
+    		user_id: context.senderId
+    	});
+        db[context.senderId] = {
+            ref: 0
+        };
+    }
+
+    context.user = db[context.senderId];
+
+    try {
+        await next();
+    } catch (err) { console.error(err) }
+});
 vk.updates.hear(/начать/i, context => (
 	context.send(
-`Здравствуй, ${vk[0].first_name} !
+`Здравствуй!
 
-Заработай 1 р за каждого реферала 💸
+Заработай 1р за каждого реферала 💸
 	
-Повышай уровень в разделе "Прогресс", чтобы зарабатывать еще больше.`)
+Повышай уровень в разделе "Прогресс", чтобы зарабатывать еще больше`)
 ));
 
 vk.updates.start().catch(console.error);
