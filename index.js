5998cb6b336872490e5843b76835d18cfdb33d77
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-18 23:22:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const vk = new VK({
 	token: process.env.TOKEN
 });
 
-vk.updates.hear(/hello/i, context => (
-	context.send('World!!')
+vk.updates.hear(/начать/i, context => (
+	context.send(
+`Здравствуй, ${vk[0].first_name} !
+
+Заработай 1 р за каждого реферала 💸
+	
+Повышай уровень в разделе "Прогресс", чтобы зарабатывать еще больше.`)
 ));
 
 vk.updates.start().catch(console.error);
