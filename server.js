04382a623f0a731103d27a6033679615536916ab
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-21 02:11:27 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const server = http.createServer();
+console.log('runned');
+server.on('request', function(request, response) {
+    response.writeHead(200);
+    console.log(request.method);
+    console.log(request.headers);
+    console.log(request.url);
+ 
+    var data = '';
+    request.on('data', function(chunk) {
+        data += chunk.toString();
+    });
+    request.on('end', function() {
+        console.log(data);
+        response.write('hi');
+        response.end();
+    });
+ 
+});
+server.listen(8080);
