a75c86e71b5addba05910e094a22678a5abac1a1
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-22 02:45:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const server = http.createServer();
+const express = require('express');
+var app = express();
 const { connect, model } = require('mongoose');
 connect(process.env.URI);
 const User = model('User', {
@@ -8,51 +8,52 @@ const User = model('User', {
 	ref: Number,
 	refed: Number
 });
-server.on('request', async function(request, response) {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', '*');
-    response.writeHead(200);
-    // console.log(request.method);
-    // console.log(request.headers);
-    // console.log(request.url);
 
-    var data = {};
-    request.on('data', function(chunk) {
-        data = JSON.parse(chunk);
-    });
-    request.on('end', async function() {
-				let user = await User.findOne({ id: data.userid });
-
-				if(!user) {
-					let $user = new User({
-						id: data.userid,
-						bal: 0,
-						ref: 0,
-						refed: 0
-					});
-
-					await $user.save();
-				}
-        response.write('hi');
-        response.end();
-    });
-		// http.request('http://vkcash.herokuapp.com/', {
-		//   method: 'POST',
-		//   headers: {
-		// 		'Accept': 'application/json',
-		//     'Content-Type': 'application/json',
-		//   },
-		// 	body: JSON.stringify({
-		// 		test: 'text'
-		// 	})
-		// })
-		// request.post('https://vk.com/app7474504', {
-		// 	json: {
-		// 		bal: 'test'
-		// 	}
-		// });
-
-
-
+app.post('/', function (req, res) {
+  res.send('Hello World!');
 });
-server.listen(process.env.PORT);
+app.listen(process.env.PORT);
+
+// server.on('request', async function(request, response) {
+//     response.setHeader('Access-Control-Allow-Origin', '*');
+//     response.setHeader('Access-Control-Allow-Headers', '*');
+//     response.writeHead(200);
+//     // console.log(request.method);
+//     // console.log(request.headers);
+//     // console.log(request.url);
+//
+//     var data = {};
+//     request.on('data', function(chunk) {
+//         data = JSON.parse(chunk);
+//     });
+//     request.on('end', async function() {
+// 				let user = await User.findOne({ id: data.userid });
+//
+// 				if(!user) {
+// 					let $user = new User({
+// 						id: data.userid,
+// 						bal: 0,
+// 						ref: 0,
+// 						refed: 0
+// 					});
+//
+// 					await $user.save();
+// 				}
+//         response.write('hi');
+//         response.end();
+//     });
+// 		http.request('http://vkcash.herokuapp.com/', {
+// 		  method: 'POST',
+// 		  headers: {
+// 				'Accept': 'application/json',
+// 		    'Content-Type': 'application/json',
+// 		  },
+// 			body: JSON.stringify({
+// 				test: 'text'
+// 			})
+// 		})
+//
+//
+//
+// });
+// server.listen(process.env.PORT);
