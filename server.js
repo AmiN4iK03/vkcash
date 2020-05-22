ad8080fc56500306b999f013cb81d6345f21ba0f
AmiN4iK03 <46399212+AmiN4iK03@users.noreply.github.com>
2020-05-22 16:32:32 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const User = model('User', {
 	refed: Number
 });
 app.use(express.json());
-app.use(function (req, res, next) {
+app.use(function (res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', '*');
 	next();
